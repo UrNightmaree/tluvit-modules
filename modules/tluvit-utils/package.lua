5d8e9d7b6265395afeb62698e8f784a872d783f5
UrNightmaree <afiqquraisyzulkarnain@gmail.com>
2022-09-24 23:40:25 +0000

    Yes

---
+++ modules/tluvit-utils/package.lua
@@ -0,0 +1,18 @@
+  return {
+    name = "tluvit-utils",
+    version = "1.0",
+    description = "A remade of luvit/utils for Tluvit",
+    tags = { 'luvit','teal','tluvit' },
+    license = "MIT",
+    author = "UrNightmaree",
+    homepage = "https://github.com/UrNightmaree/tluvit-modules",
+    dependencies = {
+      'UrNightmaree/tluvit-pretty-print'
+    },
+    files = {
+      '**.tl',
+      "**.lua",
+      "!test*"
+    }
+  }
+  
